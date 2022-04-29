9f8471dd13729e3fe9a07b2b0376dcc61f423c45
Offirmo <offirmo.net@gmail.com>
2022-04-29 11:23:16 +1000

    +++

--- xyz/functions/hello-world-alt.js
+++ xyz/functions/hello-world-alt.js
@@ -1,5 +1,10 @@
+function $parcel$export(e, n, v, s) {
+  Object.defineProperty(e, n, {get: v, set: s, enumerable: true, configurable: true});
+}
+
+$parcel$export(module.exports, "handler", () => $23124b39989247bb$export$c3c52e219617878);
 ////////////////////////////////////
-const $5465732c4170ebb4$export$c3c52e219617878 = async (event, badly_typed_context)=>{
+const $23124b39989247bb$export$c3c52e219617878 = async (event, badly_typed_context)=>{
     const msg = `Hello from netlify 2!`;
     console.log('(console.log)', msg);
     console.info('(console.info)', msg);
@@ -13,4 +18,3 @@ const $5465732c4170ebb4$export$c3c52e219617878 = async (event, badly_typed_conte
 };
 
 
-export {$5465732c4170ebb4$export$c3c52e219617878 as handler};
--- xyz/functions/hello-world.js
+++ xyz/functions/hello-world.js
@@ -1,5 +1,10 @@
+function $parcel$export(e, n, v, s) {
+  Object.defineProperty(e, n, {get: v, set: s, enumerable: true, configurable: true});
+}
+
+$parcel$export(module.exports, "handler", () => $5e472f089da6c671$export$c3c52e219617878);
 ////////////////////////////////////
-const $b915829f7022294e$export$c3c52e219617878 = async (event, badly_typed_context)=>{
+const $5e472f089da6c671$export$c3c52e219617878 = async (event, badly_typed_context)=>{
     const msg = `Hello from netlify 1!`;
     console.log('(console.log)', msg);
     console.info('(console.info)', msg);
@@ -13,4 +18,3 @@ const $b915829f7022294e$export$c3c52e219617878 = async (event, badly_typed_conte
 };
 
 
-export {$b915829f7022294e$export$c3c52e219617878 as handler};
